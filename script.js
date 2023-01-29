6f700f1006541ca21d490af3fb12b041d569e21d
Ramon Ramos <ramonramosrmt@gmail.com>
2023-01-29 21:29:24 +0800

    added dark body  theme on mobile-nav active

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector(".hamburger-container");
 const mobile_nav = document.querySelector(".mobile-nav");
+const dark = document.createElement("div");
+dark.id = "dark";
+
+document.body.appendChild(dark);
 
 menu.addEventListener("click", function (e) {
   menu.classList.toggle("is-active");
   mobile_nav.classList.toggle("is-active");
+  dark.classList.toggle("is-active");
 });
