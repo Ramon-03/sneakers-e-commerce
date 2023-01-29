4fea7ee40edce379f9c5b480e2b9a2f7a391be31
Ramon Ramos <ramonramosrmt@gmail.com>
2023-01-29 20:59:32 +0800

    added mobile nav menu and animations

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menu = document.querySelector(".hamburger-container");
+const mobile_nav = document.querySelector(".mobile-nav");
 
 menu.addEventListener("click", function (e) {
   menu.classList.toggle("is-active");
+  mobile_nav.classList.toggle("is-active");
 });
