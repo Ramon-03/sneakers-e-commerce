29b675314c5d9117ed6417062ee8b0de4e678f20
Ramon Ramos <ramonramosrmt@gmail.com>
2023-01-30 23:45:12 +0800

    fixed issue with the dark bg when present

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const menu = document.querySelector(".hamburger-container");
 const mobile_nav = document.querySelector(".mobile-nav");
-const dark = document.createElement("div");
-dark.id = "dark";
-
-document.body.appendChild(dark);
+const dark = document.querySelector(".dark");
 
 menu.addEventListener("click", function (e) {
   menu.classList.toggle("is-active");
