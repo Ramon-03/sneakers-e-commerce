5a410658b2b18fd5f22b2cbc84052a360a466998
Ramon Ramos <ramonramosrmt@gmail.com>
2023-02-01 20:47:04 +0800

    updated functionality of images

--- script.js
+++ script.js
@@ -2,27 +2,42 @@ const menu = document.querySelector(".hamburger-container");
 const mobile_nav = document.querySelector(".mobile-nav");
 const dark = document.querySelector(".dark");
 const buttons = document.querySelectorAll("[data-slider-button]");
-const thumbnail = document.querySelectorAll(".thumbnail");
+const thumbnail = document.querySelectorAll(".img-thumb");
 const lightbox = document.querySelector(".lightbox");
 const close_button = document.querySelector(".close");
 const lightbox_thumb = document.querySelectorAll(".lb-img-thumb");
-/* --------- lightbox activation --------- */
+const main_image = document.querySelectorAll(".main-image");
+
+/* --------- main-image slider --------- */
 thumbnail.forEach((thumbnail) => {
   thumbnail.addEventListener("click", (e) => {
+    const slides = document.querySelector("[data-main-slides]");
+    const activeSlide = slides.querySelector("[data-main-active]");
+    if (e.target.classList.contains("thumb-1")) {
+      delete activeSlide.dataset.mainActive;
+      slides.children[0].dataset.mainActive = true;
+    } else if (e.target.classList.contains("thumb-2")) {
+      delete activeSlide.dataset.mainActive;
+      slides.children[1].dataset.mainActive = true;
+    } else if (e.target.classList.contains("thumb-3")) {
+      delete activeSlide.dataset.mainActive;
+      slides.children[2].dataset.mainActive = true;
+    } else {
+      delete activeSlide.dataset.mainActive;
+      slides.children[3].dataset.mainActive = true;
+    }
+  });
+});
+/* --------- lightbox activation --------- */
+main_image.forEach((image) => {
+  image.addEventListener("click", (e) => {
     lightbox.classList.add("is-active");
     const slides = document.querySelector("[data-slides]");
     const activeSlide = slides.querySelector("[data-active]");
     delete activeSlide.dataset.active;
     slides.children[0].dataset.active = true;
-    const container = document
-      .querySelector(".thumb-1")
-      .closest(".lightbox-thumbnail");
-    const thumbnail = document.querySelector(".thumb-1");
-    thumbnail.classList.add("opaque");
-    container.classList.add("active");
   });
 });
-
 /* --------- lightbox deactivation --------- */
 lightbox.addEventListener("click", (e) => {
   console.log(e.target);
